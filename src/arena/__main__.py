9733f1fb3ed2dea4dc5c3e6cf44ffc09ef76021c
Tony Lechner <tlechner@atlassian.com>
2025-02-06 11:37:04 -0600

    Add sensible default logging config

--- src/arena/__main__.py
+++ src/arena/__main__.py
@@ -1,9 +1,52 @@
 from __future__ import annotations
 
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
-    pass
+    _init_logging()
+
+
+def _init_logging() -> None:
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": " ".join(
+                        [
+                            "[pid %(process)d]",
+                            "%(asctime)s",
+                            "%(levelname)s",
+                            "%(name)s",
+                        ]
+                    )
+                    + "\t%(message)s",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                },
+            },
+            "root": {"level": "WARNING", "handlers": ["default"]},
+            "loggers": {
+                **{
+                    pkg: {
+                        "level": "DEBUG",
+                        "handlers": ["default"],
+                        "propagate": False,
+                    }
+                    for pkg in ("arena", "test", "__main__")
+                }
+            },
+        }
+    )
 
 
 if __name__ == "__main__":
-    pass
+    main()
