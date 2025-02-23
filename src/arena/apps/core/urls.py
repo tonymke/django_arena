9454492a16802136255f41e69da9baaf47b35ee2
Tony Lechner <tony@tony-lechner.com>
2025-02-23 17:34:57 -0600

    Add templating boilerplate, "Hello world" index

--- src/arena/apps/core/urls.py
+++ src/arena/apps/core/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("healthcheck", views.healthcheck, name="healthcheck"),
 ]
--- src/arena/apps/core/views.py
+++ src/arena/apps/core/views.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/index.html")
 
 
 def healthcheck(request: HttpRequest) -> HttpResponse:
