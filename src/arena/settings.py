ec6f04b4cd2b84cbc1735dd18c5f1075c87dc967
Tony Lechner <tlechner@atlassian.com>
2025-02-24 14:30:26 -0600

    Move sqlite db to checkout root

--- src/arena/settings.py
+++ src/arena/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = "arena.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR.parent / "db.sqlite3",  # (checkout-root)/db.sqlite3
     }
 }
 
