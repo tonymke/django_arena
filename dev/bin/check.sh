4875518010603ace49e039592ce45bddaf8a2c59
Tony Lechner <tlechner@atlassian.com>
2026-01-08 12:05:20 -0600

    Add dryrun to checks' makemigrations

--- dev/bin/check.sh
+++ dev/bin/check.sh
@@ -10,5 +10,5 @@ uv run isort --check src tests
 uv run flake8 src tests
 uv run mypy src tests
 uv run python manage.py check
-uv run python manage.py makemigrations --check
+uv run python manage.py makemigrations --check --dry-run
 uv run pytest tests
